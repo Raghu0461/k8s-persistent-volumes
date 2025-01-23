4da1bad38cabec8afa03b93f22d589ce78c4dc10
Raghu0461 <44958979+Raghu0461@users.noreply.github.com>
2025-01-23 09:10:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-# Check if python3 is installed, and install if not
-if ! command -v python3 &> /dev/null
-then
-    echo "Python3 could not be found, installing..."
-    apt-get update && apt-get install -y python3
-fi
-
 # Run your Python script or whatever command you need
 python3 add.py config.ini
